e51d740e43162a2a03530b77d7113d5eb0e548b1
trevor <ballardt@knights.ucf.edu>
2016-11-14 15:54:29 -0500

    Refactor visualization

--- Visualization/visualize.py
+++ Visualization/visualize.py
@@ -9,105 +9,134 @@ from ete3 import PhyloTree, TreeStyle
 from Bio import Entrez
 
 Entrez.email = 'ballardt@knights.ucf.edu'
-CELLULAR_ORGANISMS_TAXID = 131567
+
 
 def parse_gra(filename, delimiter='\t'):
-	"""Parse a gra file containing taxids and relative abundances
+	"""
+	Parse a gra file containing taxids, relative abundances, and errors
 
-	args
-		filename 	- .gra file to parse
-		delimiter	- delimiter used in the .gra file. Default is tab.
+	Args:
+		filename (string): The name of the .gra file to parse.
+		delimiter (string): The delimiter used in the .gra file. Default is tab.
 
-	return
-		dictionary with taxids as keys, relative abundances as values
+	Returns:
+		A dictionary of the form:
+		{
+			<<taxid (string)>>: {
+				rel_abund: <<rel_abund (float)>>, 
+				error: <<error (float)>>
+			}
+		}
 	"""
 	with open(filename, 'rb') as f:
 		reader = csv.reader(f, delimiter=delimiter)
-		l = list(reader)[:2] # only want taxids and rel. abund.
-		return dict(zip(map(int, l[0]), map(float, l[1])))
+		l = list(reader)
+		taxids = l[0]
+		rel_abunds = map(float, l[1])
+		errors = map(float, l[2])
+		data = [{'rel_abund': r, 'error': e} for r, e in zip(rel_abunds, errors)]
+		return dict(zip(taxids, data))
 
 
-def get_lineages(taxids):
-	"""Generate the lineages for the supplied taxids.
-
-	args
-		taxids - taxids to get lineages for
-
-	return
-		dictionary with taxids as keys, list of lineage taxids as values from closest
-		ancestor to farthest
+def construct_taxonomic_tree(taxids, lineage_query_func):
 	"""
-	lineages = {}
-	memo = {}
-	for taxid in taxids:
-		handle = Entrez.efetch(db='taxonomy', id=[str(taxid)], mode='text', rettype='xml')
-		time.sleep(1) # To avoid Entrez from resetting our connection
-		taxon = Entrez.read(handle)[0] # Can read return a >1 element list here?
-		lineage = taxon['Lineage'].split('; ')
-		lineage.reverse()
-		lineages[taxid] = list(species_to_taxids(lineage, memo))
-	return lineages
+	Create a taxonomic tree from a list of taxids and arbitrary data
 
+	Args:
+		taxids ([string]): The list of taxids that represent leaf nodes
+		lineage_query_func (func): A function that produces a list of lineage taxids
+				given a taxid
 
-def species_to_taxids(species_names, memo={}):
-	"""Given a list of species names, get their corresponding taxids.
-
-	args
-		species_names - list of species names to convert to taxids
-		memo - dictionary of species calculated thus far (for DP)
-
-	return
-		list of taxids for each species
+	Returns:
+		The root of the created tree
 	"""
-	for species in species_names:
-		species = species.replace(' ', '+').strip()
-		if species not in memo:
-			search = Entrez.esearch(term=species, db='taxonomy', retmode='xml')
-			memo[species] = int(Entrez.read(search)['IdList'][0])
-		yield memo[species]
 
-
-def construct_phylo_tree(taxids, rel_abunds=None):
-	"""Create a phylogenetic with relative abundance info from a list of taxids
-
-	args
-		taxids - list of taxids
-		rel_abunds - dictionary of relative abundances for taxids
-
-	return
-		root of the created tree
-	"""
+	# Keeping track of nodes made thus far allows us to create branches on the
+	# tree and memoize
 	nodes = {}
-	lineages = get_lineages(taxids)
-	for taxid, lineage in lineages.iteritems():
-		path = [new_node(taxid, nodes)]
-		child = path[0]
-		for ancestor in lineage:
-			path.append(new_node(ancestor, nodes))
-			if child not in path[-1].children:
-				path[-1].add_child(child)
-			child = path[-1]
-	return nodes[CELLULAR_ORGANISMS_TAXID]
+
+	# The element that will be returned; the root of the tree
+	root = None
+
+	# Go through each organism in the sample, fetching its lineage and creating a
+	# node for it
+	for taxid in taxids:
+		lineage = lineage_query_func(taxid)
+		child = tax_node(taxid)
+
+		# For each ancestor of this sample organism that we haven't seen, create a
+		# node for it and add the previous node as a child. This creates a path from
+		# the leaf to the most ancestral node not yet created.
+		for ancestor in (l for l in lineage if l not in nodes):
+			nodes[ancestor] = tax_node(ancestor)
+			nodes[ancestor].add_child(child)
+			child = nodes[ancestor]
+
+		# If we have a new root, set it. Otherwise, connect the most recently 
+		# created node to the rest of the tree by attaching it to its parent.
+		# TODO this doesn't seem like a great way of doing it
+		if child.taxid == lineage[-1]:
+			root = child
+		else:
+			parentIndex = lineage.index(child.taxid) + 1
+			parent = nodes[lineage[parentIndex]]
+			parent.add_child(child)
+
+	return root
 
 
-def new_node(taxid, nodes={}):
-	if taxid not in nodes:
-		node = nodes.setdefault(taxid, PhyloTree())
-		node.name = taxid
-		node.taxid = taxid
-	return nodes[taxid]
+def tax_node(taxid):
+	"""
+	Create a node on the taxonomic tree
+
+	Args:
+		taxid (string): The taxid of the node
+
+	Returns:
+		The newly created node
+	"""
+	node = PhyloTree()
+	node.name = taxid
+	node.taxid = taxid
+	return node
+
+
+def get_lineage_entrez(taxid):
+	"""
+	Fetch a taxid's lineage on-demand via Entrez
+
+	Args:
+		taxid (string): taxid of the organism to get the lineage of
+
+	Returns:
+		A list with the lineage taxids ([string])
+	"""
+	time.sleep(1) # To avoid Entrez from resetting our connection
+	handle = Entrez.efetch(db='taxonomy', id=[taxid], mode='text', rettype='xml')
+	[taxon] = Entrez.read(handle)
+	lineage = taxon['Lineage'].split('; ')
+	lineage.reverse()
+	return [name_to_taxid(s) for s in lineage]
+
+
+def name_to_taxid(name):
+	"""
+	Convert a species name to a taxid
+
+	Args:
+		name (string): The name of the organism in NCBI's taxonomy database
+
+	Returns:
+		The taxid of the organism (string)
+	"""
+	name = name.replace(' ', '+').strip()
+	search = Entrez.esearch(term=name, db='taxonomy', retmode='xml')
+	return Entrez.read(search)['IdList'][0]
 
 
 if __name__ == "__main__":
 	gra_filename = sys.argv[1]
-	rel_abunds = parse_gra(gra_filename)
-	t = construct_phylo_tree(rel_abunds.keys())
+	sample_organisms = parse_gra(gra_filename)
+	taxids = sample_organisms.keys()
+	t = construct_taxonomic_tree(taxids, get_lineage_entrez)
 	t.render('phylo.png', tree_style=TreeStyle())
-
-	"""
-	# Load a tree and link it to an alignment.
-	t = PhyloTree("(((seqA,seqB),seqC),seqD);")
-	t.link_to_alignment(alignment=fasta_txt, alg_format="fasta")
-
-	t.render('phylo.png');
-	"""
