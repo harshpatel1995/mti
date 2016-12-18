cbe383f779566a05e88cea847ae78114e787835f
trevor <ballardt@knights.ucf.edu>
2016-12-18 18:41:31 -0500

    Remove charade GRAMMy, rename GRAMMy folder

--- charade-grammy/grammy/mydebug.cpp
+++
@@ -1,9 +0,0 @@
-/**************************
-*Debug Helper Model Source
-***************************/
-#ifndef _GEM_MYDEBUG_C
-#define _GEM_MYDEBUG_C
-
-#include "mydebug.hpp"
-
-#endif _GEM_MYDEBUG_C
