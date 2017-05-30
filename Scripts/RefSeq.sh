1782feeae0b82e4886159beaece70059aab35647
Harsh Patel <patelh@coombs.cs.ucf.edu>
2017-05-30 10:57:53 -0400

    NCBI Genome FASTA Fetch Script

---
+++ Scripts/RefSeq.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+declare -a genomes_of_interest=("archaea" "bacteria" "viral")
+
+function download {
+	for i in "${genomes_of_interest[@]}"
+	do
+		rm -r ../../genomes/"$i"
+		mkdir ../../genomes/"$i"
+		cd ../../genomes/"$i"
+		curl "ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/${i}/assembly_summary.txt" | \
+		awk '{FS="\t"} !/^#/ {print $20} ' | \
+		sed -r 's|(ftp://ftp.ncbi.nlm.nih.gov/genomes/all/.+/)(GCF_.+)|\1\2/\2_genomic.fna.gz|' > genomic_file
+		mkdir fasta
+		cd fasta/
+		wget --input ../genomic_file
+		find . -type f -exec gunzip {} +  	
+		cd ../../../mti/Scripts/
+	done
+}
+
+function index {
+	cd ../../genomes/
+	rm reference.fna
+	find . -name '*.fna' -exec cat {} + > reference.fna | tee reference.fna
+}
+
+download
+index
